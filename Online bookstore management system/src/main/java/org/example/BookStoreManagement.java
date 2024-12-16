9556ab69afd1ab33772597bb6c9328476c22faac
sultan <zyldyzbekovsultan02@gmail.com>
2024-12-16 12:28:09 +0600

    Bookstoremanagement

---
+++ Online bookstore management system/src/main/java/org/example/BookStoreManagement.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class BookStoreManagement {
+
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    static final String DB_USER = "postgres";
+    static final String DB_PASSWORD = "1234";
+}
+
