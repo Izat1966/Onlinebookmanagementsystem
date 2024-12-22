d55b0e9815400fef1618acce4894723a6ca11b7e
Izat1966 <150505243+Izat1966@users.noreply.github.com>
2024-12-22 22:17:45 +0600

    Update BookStoreManagement.java

--- Online bookstore management system/src/main/java/org/example/BookStoreManagement.java
+++ Online bookstore management system/src/main/java/org/example/BookStoreManagement.java
@@ -7,6 +7,6 @@ public class BookStoreManagement {
 
     static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
     static final String DB_USER = "postgres";
-    static final String DB_PASSWORD = "1234";
+    static final String DB_PASSWORD = "220506";
 }
 
